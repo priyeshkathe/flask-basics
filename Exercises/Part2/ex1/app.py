5fe487dc6e115463d8ba760e1ee507e8e2dcc1dd
Priyesh Kathe <priyeshkathe@gmail.com>
2026-01-18 19:38:49 +0530

    part 2 exercise

---
+++ Exercises/Part2/ex1/app.py
@@ -0,0 +1,25 @@
+# Exercise 2.1: Modify the templates
+#   - Edit index.html and add more content
+#   - Refresh browser to see changes
+
+from flask import Flask, render_template 
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')  
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')  
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')  
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
